563cd7c651775156c561f849c0cfcbe427c858af
Caroline Ann Rajesh <caroline.rba2529@saintgits.org>
2026-06-11 14:00:33 +0530

    Initial Commit

---
+++ blink/blink/main.c
@@ -0,0 +1,24 @@
+/*
+ * blink.c
+ *
+ * Created: 6/8/2026 11:50:15 AM
+ * Author : TU33
+ */ 
+#define F_CPU 16000000UL
+#include <avr/io.h>
+#include <util/delay.h>
+
+
+int main(void)
+{
+	DDRB=0b00100000;
+    /* Replace with your application code */
+    while (1) 
+    {
+		PORTB=0b00100000;
+		_delay_ms(1000);
+		PORTB=0b00000000;
+		_delay_ms(1000);
+    }
+}
+
